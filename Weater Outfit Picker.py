9f565fd4025bce8e2735f1e9f5801bdaa82bb9fc
Shaurya030825 <shaurya030825@gmail.com>
2026-09-12 10:43:48 +0530

    Add files via upload

---
+++ Weater Outfit Picker.py
@@ -0,0 +1,54 @@
+temperature= int(input("Enter today's temperature in Celcius:"))
+
+if temperature < 20:
+    outfit= "jacket"
+    print("It's cold today.")
+    print("Wear a", outfit)
+
+else:
+    outfit="t-shirt"
+    print("It's warm today.")
+    print("Wear a", outfit)
+
+is_raining= input("Is it raining today?(yes/no)")
+
+if is_raining== "yes":
+    print("Bring an umbrella!")
+
+wind_speed= int(input("Enter the wind speed in km/hr"))
+
+if wind_speed>30:
+    needs_windbreaker= "yes"
+    print("It is windy today.")
+    print("Wear a windbreaker over your", outfit)
+
+else:
+   needs_windbreaker = "no"
+   print("It is calm today.")
+   print("No windbreaker needed over your", outfit)
+
+has_puddles= input("Are there puddles on the ground?(yes/no):")
+
+if has_puddles == "yes":
+    shoes= "boots"
+    print("The ground is wet.")
+    print("Wear", shoes)
+
+else:
+    shoes = "sneakers"
+    print("The ground is dry.")
+    print("Wear", shoes)
+
+print("")
+print("Weather check complete!")
+
+print("=====WEATHER OUTFIT PICKER==========")
+print("Temperature:", temperature)
+print("Outfit chosen:", outfit)
+print("Raining:", is_raining)
+print("Windbreaker needed:",needs_windbreaker)
+print("Shoes chosen:",shoes)
+print("==================================") 
+
+
+
